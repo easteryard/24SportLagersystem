d0c7eea8ea1d412dd9d8a937d7bfa480c183ddfe
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-05 13:04:50 +0200

    Created ProductPart class and added props, constructor and tostring

---
+++ 24SportLagersystem/24SportLagersystem/Model/ProductPart.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SportLagersystem.Model
+{
+    class ProductPart
+    {
+        public int ProductPartId { get; set; }
+        public int Amount { get; set; }
+        public string Description { get; set; }
+        public double PricePrPieceEuro { get; set; }
+        public double PricePrPieceDKK { get; set; }
+        public double PriceTotalEuro { get; set; }
+        public double PriceTotalDKK { get; set; }
+
+        public ProductPart(int productPartId, int amount, string description, double pricePrPieceEuro, double pricePrPieceDkk, double priceTotalEuro, double priceTotalDkk)
+        {
+            ProductPartId = productPartId;
+            Amount = amount;
+            Description = description;
+            PricePrPieceEuro = pricePrPieceEuro;
+            PricePrPieceDKK = pricePrPieceDkk;
+            PriceTotalEuro = priceTotalEuro;
+            PriceTotalDKK = priceTotalDkk;
+        }
+
+        public ProductPart()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(ProductPartId)}: {ProductPartId}, {nameof(Amount)}: {Amount}, {nameof(Description)}: {Description}, {nameof(PricePrPieceEuro)}: {PricePrPieceEuro}, {nameof(PricePrPieceDKK)}: {PricePrPieceDKK}, {nameof(PriceTotalEuro)}: {PriceTotalEuro}, {nameof(PriceTotalDKK)}: {PriceTotalDKK}";
+        }
+    }
+}
