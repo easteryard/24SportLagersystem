3717cc89592066cc306d2b4ef3280acc07135dce
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-05 12:23:50 +0200

    tilføjet customer class

---
+++ 24SportLagersystem/24SportLagersystem/Converter/DateTimeConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SportLagersystem.Converter
+{
+    class DateTimeConverter
+    {
+        public static DateTime DateTimeOffsetAndTimeSetToDateTime(DateTimeOffset date, TimeSpan time)
+        { return new DateTime(date.Year, date.Month, date.Day, time.Hours, time.Minutes, 0); }
+    }
+}
