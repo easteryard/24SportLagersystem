569ad9e32cb13b4310226a34eff998efefda4a8b
Xentrosh <fabianwendell@hotmail.com>
2017-05-05 10:09:21 +0200

    ændret Relay

--- 24SportLagersystem/24SportLagersystem/Common/RelayArgCommand.cs
+++ 24SportLagersystem/24SportLagersystem/Common/RelayArgCommand.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace Eventmaker.Common
+namespace _24SportLagersystem.Common
 {
     class RelayArgCommand<T> : ICommand
     {
