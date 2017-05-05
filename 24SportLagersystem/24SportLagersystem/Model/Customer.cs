35caf45a98b38b09c2a1a0eef10205e106b58dad
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-05 12:49:11 +0200

    Added props, constuctor and tostring to Customer class

--- 24SportLagersystem/24SportLagersystem/Model/Customer.cs
+++ 24SportLagersystem/24SportLagersystem/Model/Customer.cs
@@ -8,5 +8,29 @@ namespace _24SportLagersystem.Model
 {
     class Customer
     {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public int PhoneNr { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        public Customer(int customerId, string name, int phoneNr, string address, string email)
+        {
+            CustomerId = customerId;
+            Name = name;
+            PhoneNr = phoneNr;
+            Address = address;
+            Email = email;
+        }
+
+        public Customer()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(CustomerId)}: {CustomerId}, {nameof(Name)}: {Name}, {nameof(PhoneNr)}: {PhoneNr}, {nameof(Address)}: {Address}, {nameof(Email)}: {Email}";
+        }
     }
 }
