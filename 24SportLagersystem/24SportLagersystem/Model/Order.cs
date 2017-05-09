2952f9082d6d1a275b81d55a156de96376450611
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-09 09:54:58 +0200

    Props added to Order class

--- 24SportLagersystem/24SportLagersystem/Model/Order.cs
+++ 24SportLagersystem/24SportLagersystem/Model/Order.cs
@@ -8,5 +8,25 @@ namespace _24SportLagersystem.Model
 {
     class Order
     {
+        public int OrderNr { get; set; }
+        public DateTime DateTime { get; set; }
+        public String Content { get; set; }
+
+        public Order()
+        {
+
+        }
+
+        public Order(int orderNr, DateTime dateTime, string content)
+        {
+            OrderNr = orderNr;
+            DateTime = dateTime;
+            Content = content;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(OrderNr)}: {OrderNr}, {nameof(DateTime)}: {DateTime}, {nameof(Content)}: {Content}";
+        }
     }
 }
