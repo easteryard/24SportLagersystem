67e54cef7940f8893ee1bb7e5fb4c4a2329340cf
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-09 10:02:08 +0200

    Code added in Product

--- 24SportLagersystem/24SportLagersystem/Model/Product.cs
+++ 24SportLagersystem/24SportLagersystem/Model/Product.cs
@@ -8,5 +8,27 @@ namespace _24SportLagersystem.Model
 {
     class Product
     {
+        public String ProductName { get; set; }
+        public String Content { get; set; }
+        public double Price { get; set; }
+        public int AmountMade { get; set; }
+
+        public Product()
+        {
+            
+        }
+
+        public Product(string productName, string content, double price, int amountMade)
+        {
+            ProductName = productName;
+            Content = content;
+            Price = price;
+            AmountMade = amountMade;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(ProductName)}: {ProductName}, {nameof(Content)}: {Content}, {nameof(Price)}: {Price}, {nameof(AmountMade)}: {AmountMade}";
+        }
     }
 }
