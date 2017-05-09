5538471c9cac0194e16eb8da5ffea393e991fcd8
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-09 10:37:31 +0200

    ProductPart now has props, constructor and tostring

--- 24SportLagersystem/24SportLagersystem/Model/ProductPart.cs
+++ 24SportLagersystem/24SportLagersystem/Model/ProductPart.cs
@@ -8,5 +8,29 @@ namespace _24SportLagersystem.Model
 {
     class ProductPart
     {
+        public int ProductPartNr { get; set; }
+        public int Amount { get; set; }
+        public string Description { get; set; }
+        public double PricePrPiece { get; set; }
+        public double TotalPrice { get; set; }
+
+        public ProductPart()
+        {
+
+        }
+
+        public ProductPart(int productPartNr, int amount, string description, double pricePrPiece, double totalPrice)
+        {
+            ProductPartNr = productPartNr;
+            Amount = amount;
+            Description = description;
+            PricePrPiece = pricePrPiece;
+            TotalPrice = totalPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(ProductPartNr)}: {ProductPartNr}, {nameof(Amount)}: {Amount}, {nameof(Description)}: {Description}, {nameof(PricePrPiece)}: {PricePrPiece}, {nameof(TotalPrice)}: {TotalPrice}";
+        }
     }
 }
