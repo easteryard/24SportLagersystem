2cf15482c2582f65c2b612eda086a3c379574952
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-09 09:47:21 +0200

    Props added in Customer

--- 24SportLagersystem/24SportLagersystem/Model/Customer.cs
+++ 24SportLagersystem/24SportLagersystem/Model/Customer.cs
@@ -8,5 +8,30 @@ namespace _24SportLagersystem.Model
 {
     class Customer
     {
+        public String Name { get; set; }
+        public int CustomerId { get; set; }
+        public int PhoneNr { get; set; }
+        public String Address { get; set; }
+        public String Email { get; set; }
+
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(string name, int customerId, int phoneNr, string address, string email)
+        {
+            Name = name;
+            CustomerId = customerId;
+            PhoneNr = phoneNr;
+            Address = address;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(CustomerId)}: {CustomerId}, {nameof(PhoneNr)}: {PhoneNr}, {nameof(Address)}: {Address}, {nameof(Email)}: {Email}";
+        }
     }
 }
