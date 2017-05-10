c1cb19d06047a69ed32b26d0e8ae6c82e82b7f9a
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-10 13:26:00 +0200

    Viewmodel and persistency

--- 24SportLagersystem/24SportLagersystem/Model/Singleton24Sport.cs
+++ 24SportLagersystem/24SportLagersystem/Model/Singleton24Sport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,36 @@ namespace _24SportLagersystem.Model
 {
     class Singleton24Sport
     {
+        public PersistencyService.PersistencyService PersistencyService { get; set; }
+
+        private static Singleton24Sport _instance = new Singleton24Sport();
+        public static Singleton24Sport Instance
+        {
+
+
+            get { return _instance ?? (_instance = new Singleton24Sport()); }
+
+        }
+
+        public ObservableCollection<Product> Products { get; set; }
+
+        private Singleton24Sport()
+        {
+            Products = new ObservableCollection<Product>();
+            //LoadProductsAsync();
+        }
+
+
+
+        //public async void LoadProductsAsync()
+        //{
+        //    var products = await PersistencyService.LoadProductFromJsonAsync();
+        //    if (products != null)
+        //        foreach (var ev in products)
+        //        {
+        //            Products.Add(ev);
+        //        }
+
+        //}
     }
 }
