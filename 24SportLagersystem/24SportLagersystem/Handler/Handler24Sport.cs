cce0314a336105d83ff185113d96890b57a2f50a
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-10 13:34:39 +0200

    code added to Handler

--- 24SportLagersystem/24SportLagersystem/Handler/Handler24Sport.cs
+++ 24SportLagersystem/24SportLagersystem/Handler/Handler24Sport.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _24SportLagersystem.ViewModel;
 
 namespace _24SportLagersystem.Handler
 {
     class Handler24Sport
     {
+        public ViewModel24Sport ViewModel24Sport { get; set; }
+
+        public Handler24Sport(ViewModel24Sport viewModel24Sport)
+        {
+            ViewModel24Sport = viewModel24Sport;
+        }
     }
 }
