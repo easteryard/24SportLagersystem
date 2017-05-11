d8d1a39df0d6b2b00810d2843a62535021135c2d
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-11 10:36:24 +0200

    Fixed method in Singleton

--- 24SportLagersystem/24SportLagersystem/Model/Singleton24Sport.cs
+++ 24SportLagersystem/24SportLagersystem/Model/Singleton24Sport.cs
@@ -4,12 +4,12 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _24SportLagersystem.PersistencyService;
 
 namespace _24SportLagersystem.Model
 {
     class Singleton24Sport
     {
-        public PersistencyService.PersistencyService PersistencyService { get; set; }
 
         private static Singleton24Sport _instance = new Singleton24Sport();
         public static Singleton24Sport Instance
@@ -25,20 +25,20 @@ namespace _24SportLagersystem.Model
         private Singleton24Sport()
         {
             Products = new ObservableCollection<Product>();
-            //LoadProductsAsync();
+            LoadProductsAsync();
         }
 
 
 
-        //public async void LoadProductsAsync()
-        //{
-        //    var products = await PersistencyService.LoadProductFromJsonAsync();
-        //    if (products != null)
-        //        foreach (var ev in products)
-        //        {
-        //            Products.Add(ev);
-        //        }
+        public async void LoadProductsAsync()
+        {
+            var products = await PersistencyService.PersistencyService.LoadProductFromJsonAsync();
+            if (products != null)
+                foreach (var ev in products)
+                {
+                    Products.Add(ev);
+                }
 
-        //}
+        }
     }
 }
