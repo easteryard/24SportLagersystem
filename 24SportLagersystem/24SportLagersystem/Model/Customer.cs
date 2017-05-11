bd0a43d940432d2779fe6db6fd4074f88be7f862
NicklasN96 <nicklas.n@nielsen.mail.dk>
2017-05-11 13:36:29 +0200

    Minor fixes in the Customer class

--- 24SportLagersystem/24SportLagersystem/Model/Customer.cs
+++ 24SportLagersystem/24SportLagersystem/Model/Customer.cs
@@ -8,30 +8,29 @@ namespace _24SportLagersystem.Model
 {
     class Customer
     {
-        public string Name { get; set; }
         public int CustomerId { get; set; }
-        public int PhoneNr { get; set; }
+        public string Name { get; set; }
+        public int PhoneNo { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
-
 
         public Customer()
         {
 
         }
 
-        public Customer(string name, int customerId, int phoneNr, string address, string email)
+        public Customer(int customerId, string name, int phoneNo, string address, string email)
         {
-            Name = name;
             CustomerId = customerId;
-            PhoneNr = phoneNr;
+            Name = name;
+            PhoneNo = phoneNo;
             Address = address;
             Email = email;
         }
 
         public override string ToString()
         {
-            return $"{nameof(Name)}: {Name}, {nameof(CustomerId)}: {CustomerId}, {nameof(PhoneNr)}: {PhoneNr}, {nameof(Address)}: {Address}, {nameof(Email)}: {Email}";
+            return $"{nameof(CustomerId)}: {CustomerId}, {nameof(Name)}: {Name}, {nameof(PhoneNo)}: {PhoneNo}, {nameof(Address)}: {Address}, {nameof(Email)}: {Email}";
         }
     }
 }
