a4bb00fddc39bfb97abd682f54fd51978a19e39d
Casper Hemmje Østergaard <cstergaard@hotmail.com>
2017-05-11 13:08:28 +0200

    Tilføjet Save CRUD i program (ikke færdig)

--- 24SportLagersystem/24SportConsole/Program.cs
+++ 24SportLagersystem/24SportConsole/Program.cs
@@ -16,40 +16,42 @@ namespace _24SportConsole
 
             //READ METHOD
 
-            const string SERVER_URL = "http://localhost:41731";
-            HttpClientHandler handler = new HttpClientHandler();
-            handler.UseDefaultCredentials = true;
-            using (var client = new HttpClient(handler))
-            {
-                client.BaseAddress = new Uri(SERVER_URL);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //const string SERVER_URL = "http://localhost:41731";
+            //HttpClientHandler handler = new HttpClientHandler();
+            //handler.UseDefaultCredentials = true;
+            //using (var client = new HttpClient(handler))
+            //{
+            //    client.BaseAddress = new Uri(SERVER_URL);
+            //    client.DefaultRequestHeaders.Clear();
+            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                try
-                {
-                    var response = client.GetAsync("api/customers").Result;
-                    if (response.IsSuccessStatusCode)
-                    {
-                        IEnumerable<Customer> customers = response.Content.ReadAsAsync<IEnumerable<Customer>>().Result;
-                        foreach (var customer in customers)
-                        {
-                            Console.WriteLine(customer);
-                        }
-                    }
+            //    try
+            //    {
+            //        var response = client.GetAsync("api/customers").Result;
+            //        if (response.IsSuccessStatusCode)
+            //        {
+            //            IEnumerable<Customer> customers = response.Content.ReadAsAsync<IEnumerable<Customer>>().Result;
+            //            foreach (var customer in customers)
+            //            {
+            //                Console.WriteLine(customer);
+            //            }
+            //        }
 
-                    //var response = client.PostAsJsonAsync("api/events", new Event()).Result;
-                    //var response = client.PutAsync("api/events/" + events.Id)
-                }
-                catch (Exception)
-                {
+            //        //var response = client.PostAsJsonAsync("api/events", new Event()).Result;
+            //        //var response = client.PutAsync("api/events/" + events.Id)
+            //    }
+            //    catch (Exception)
+            //    {
 
-                    throw;
-                }
-            }
+            //        throw;
+            //    }
+            //}
+
+            SaveEventsAsJsonAsync(new Customer("Karsten", 0, 12345678, "Vejen 1, 4000", "mail@mail.dk"));
         }
 
 
-        public static async void SaveEventsAsJsonAsync(Event events)
+        public static async void SaveEventsAsJsonAsync(Customer customer)
         {
             const string SERVER_URL = "http://localhost:3683";
             HttpClientHandler handler = new HttpClientHandler();
@@ -63,16 +65,17 @@ namespace _24SportConsole
 
                 try
                 {
-                    var response = client.PostAsJsonAsync("api/Events", events).Result;
+                    var response = client.PostAsJsonAsync("api/Customers", customer).Result;
                     if (response.IsSuccessStatusCode)
                     {
-                        var returnEvent = response.Content.ReadAsAsync<Event>();
-                        events.Id = returnEvent.Result.Id;
+                        var returnEvent = response.Content.ReadAsAsync<Customer>();
+                        customer.Customer_Id = returnEvent.Result.Customer_Id;
                     }
                 }
                 catch (Exception ex)
                 {
-                    new MessageDialog(ex.Message).ShowAsync();
+                    throw;
+                    //new MessageDialog(ex.Message).ShowAsync();
                 }
             }
         }
