9d9e5200349ff49f54dc219a3a1f617d6eff419d
Niclas Krabbe Kristensen <krabbe89@gmail.com>
2017-05-24 13:35:52 +0200

    dsa

--- 24SportLagersystem/24SportLagersystem/ViewModel/ViewModel24Sport.cs
+++ 24SportLagersystem/24SportLagersystem/ViewModel/ViewModel24Sport.cs
@@ -300,10 +300,10 @@ namespace _24SportLagersystem.ViewModel
             Singleton24Sport = Singleton24Sport.Instance;
             DateTime dt = System.DateTime.Now;
 
-            _orderDate = new DateTimeOffset(dt.Year, dt.Month, dt.Day, 0, 0, 0, 0, new TimeSpan());
-            _deliveryDate = new DateTimeOffset(dt.Year, dt.Month, dt.Day, 0, 0, 0, 0, new TimeSpan());
-            _timeSpanDeliveryDate = new TimeSpan(dt.Hour, dt.Minute, dt.Second);
-            _timeSpanOrderDate = new TimeSpan(dt.Hour, dt.Minute, dt.Second);
+            _orderDate = new DateTimeOffset(2018, 6, dt.Day, 0, 0, 0, 0, new TimeSpan());
+            _deliveryDate = new DateTimeOffset(2018, 6, dt.Day, 0, 0, 0, 0, new TimeSpan());
+            //_timeSpanDeliveryDate = new TimeSpan(dt.Hour, dt.Minute, dt.Second);
+            //_timeSpanOrderDate = new TimeSpan(dt.Hour, dt.Minute, dt.Second);
 
             EditOrderCommand = new RelayCommand(Handler24Sport.EditOrder);
             ToOrderEditPagecCommand = new RelayCommand(NavigateToEditEventPage);
