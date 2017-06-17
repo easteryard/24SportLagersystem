d433900d97036cd2cf73dd2b794e3acdc07787c7
Xentrosh <fabianwendell@hotmail.com>
2017-06-17 23:02:03 +0200

    ..

--- 24SportLagersystem/24SportLagersystem/Handler/Handler24Sport.cs
+++ 24SportLagersystem/24SportLagersystem/Handler/Handler24Sport.cs
@@ -18,30 +18,7 @@ namespace _24SportLagersystem.Handler
         {
             ViewModel24Sport = viewModel24Sport;
         }
-/*
-        public void CreateProduct()
-        {
-            ViewModel24Sport.Singleton24Sport.AddProduct(ViewModel24Sport.ProductId, ViewModel24Sport.ProductName,
-                ViewModel24Sport.Price, ViewModel24Sport.Height, ViewModel24Sport.AmountMade,
-                ViewModel24Sport.AmountMakeable);
-        }
-
-        public void CreateProductPart()
-        {
-            ViewModel24Sport.Singleton24Sport.AddProductPart(ViewModel24Sport.ProductPartId, ViewModel24Sport.ProductPartNo, ViewModel24Sport.Description, ViewModel24Sport.Amount, ViewModel24Sport.PricePerDkk, ViewModel24Sport.PricePerEur, ViewModel24Sport.PriceTotalDkk, ViewModel24Sport.PriceTotalEur);
-        }
-
-        public void CreateCustomer()
-        {
-            ViewModel24Sport.Singleton24Sport.AddCustomer(ViewModel24Sport.CustomerId, ViewModel24Sport.Name, ViewModel24Sport.PhoneNo, ViewModel24Sport.Address, ViewModel24Sport.Email);
-        }
-        */
-        //public void CreateOrder()
-        //{
-        //    ViewModel24Sport.Singleton24Sport.AddOrder(ViewModel24Sport.OrderId, DateTimeConverter.DateTimeOffsetAndTimeSetToDateTime(ViewModel24Sport.OrderDate, ViewModel24Sport.TimeSpanOrdreDate), DateTimeConverter.DateTimeOffsetAndTimeSetToDateTime(ViewModel24Sport.DeliveryDate, ViewModel24Sport.TimeSpanDeliveryDate), ViewModel24Sport.CustomerId);
-        //}
-        
-
+/
         public void SetSelectedOrder(Order order)
         {
             ViewModel24Sport.SelectedOrder = order;
